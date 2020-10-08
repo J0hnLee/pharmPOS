9025ce81e27d55f6aa192dcfe8cda243dedc2a7e
john791016 <john791016@gmail.com>
2020-10-09 01:12:40 +0800

    CRUD method

---
+++ api/CRUD.py
@@ -0,0 +1,32 @@
+from api import db, Prescription
+
+##CREATE##
+prescription = Prescription('亞東','kerker1','25')
+db.session.add(prescription)
+db.session.commit()
+
+##READ##
+all_prescription=Prescription.query.all() # list of prescriptions objects in the talbe.
+print(all_prescription)
+
+##SELECT BY ID##
+prescriptionOne=Prescription.query.get(1)
+print(prescriptionOne.patientName)
+
+#FILTERS
+#PRODUCE SOME SQL CODE
+sampleOne=Prescription.query.filter_by(patientAge=25)
+print(sampleOne.all())
+
+
+####UPDATE
+firstSample= Prescription.query.get(1)
+firstSample.age=10
+db.session.add(firstSample)
+db.session.commit()
+
+
+###DELETE
+secondSample=Prescription.query.get(2)
+db.session.delete(secondSample)
+db.session.commit()
--- api/setupDataBase.py
+++ api/setupDataBase.py
@@ -4,8 +4,8 @@ from api import db, Prescription
 # CREATES ALL THE TABLES Model--> Db table
 db.create_all()
 
-sample1=Prescription('亞東','kerker1','25')
-sample2=Prescription('亞東','kerker3','52')
+sample1=Prescription('亞東','kerker1',25)
+sample2=Prescription('亞東','kerker3',52)
 
 print(sample1.id)
 print(sample2.id)
